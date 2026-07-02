3e748ead4591399dfee1d6efb9f64c8f707ed7be
RJZC <356716232@qq.com>
2026-07-02 17:58:18 +0800

    Update YK.js

--- QiXingCang/data/js/YK.js
+++ QiXingCang/data/js/YK.js
@@ -2,13 +2,11 @@ var rule = {
     title: '优酷',
     host: 'https://www.%79%6f%75%6b%75.com',
     homeUrl: '',
-    searchUrl: 'https://search.%79%6f%75%6b%75.com/api/search?pg=fypage&keyword=**',
-    searchable: 0,
-    quickSearch: 0,
-    "indexs": 1,
+    searchUrl: '',
+    searchable: 1,
+    quickSearch: 1,
+    "indexs": 0,
     filterable: 1,
-    multi: 1,
-    url: '/category/data?optionRefresh=1&pageNo=fypage&params=fyfilter',
     filter_url: '{{fl}}',
     filter: {
         "电视剧": [{
