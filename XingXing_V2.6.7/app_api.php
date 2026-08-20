55c35e14137dc8773aa7c8e1bd789554e0caa0a3
RJZC <356716232@qq.com>
2026-08-20 18:44:47 +0800

    Update app_api.php

--- XingXing_V2.6.7/app_api.php
+++ XingXing_V2.6.7/app_api.php
@@ -1,1 +1,8 @@
-{"storeHouse":[{"sourceName":"StoreHouse","sourceUrl":"https://gh-proxy.com/https://raw.githubusercontent.com/RJZC-LRJ/RJ/refs/heads/main/XingXing_V2.6.7/api_url.json"}]}
+{
+  "storeHouse": [
+    {
+      "sourceName": "StoreHouse",
+      "sourceUrl": "https://gh-proxy.com/https://raw.githubusercontent.com/RJZC-LRJ/RJ/refs/heads/main/XingXing_V2.6.7/api_url.json"
+    }
+  ]
+}
