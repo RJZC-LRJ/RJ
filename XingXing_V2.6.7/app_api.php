6c8e13421371b17378ea5b9e0b66b86dbdb7ceb7
RJZC <356716232@qq.com>
2026-09-04 15:18:28 +0800

    Delete XingXing_V2.6.7 directory

--- XingXing_V2.6.7/app_api.php
+++
@@ -1,8 +0,0 @@
-{
-  "storeHouse": [
-    {
-      "sourceName": "StoreHouse",
-      "sourceUrl": "https://gh-proxy.com/https://raw.githubusercontent.com/RJZC-LRJ/RJ/refs/heads/main/XingXing_V2.6.7/api_url.json"
-    }
-  ]
-}
